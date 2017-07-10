e331566d7ce80fb7eb12030cd925eff9b3afd978
Kong Lee <konglee873@gmail.com>
2017-07-10 18:00:12 +0800

    save code
    类型转换

---
+++ CPP_Basic_Template/TypeTransfer/TypeTransfer.cpp
@@ -0,0 +1,9 @@
+//
+//  TypeTransfer.cpp
+//  CPP_Basic_Template
+//
+//  Created by kong on 2017/7/10.
+//  Copyright © 2017年 konglee. All rights reserved.
+//
+
+#include "TypeTransfer.hpp"
---
+++ CPP_Basic_Template/TypeTransfer/TypeTransfer.hpp
@@ -0,0 +1,14 @@
+//
+//  TypeTransfer.hpp
+//  CPP_Basic_Template
+//
+//  Created by kong on 2017/7/10.
+//  Copyright © 2017年 konglee. All rights reserved.
+//
+
+#ifndef TypeTransfer_hpp
+#define TypeTransfer_hpp
+
+#include <stdio.h>
+
+#endif /* TypeTransfer_hpp */
